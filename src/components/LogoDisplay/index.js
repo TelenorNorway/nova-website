bcdf55b1653d270bc50b77b154e8b7ae2c635df5
Eline Henriksen <eline.henriksen@telenor.no>
2025-01-22 12:23:21 +0100

    Use white logo

--- src/components/LogoDisplay/index.js
+++ src/components/LogoDisplay/index.js
@@ -3,7 +3,7 @@ import * as styles from "./LogoDisplay.module.css"
 
 const LogoDisplay = () => (
     <div className={styles.logoFlex}>
-        <img src="/logo.png" alt="Telenor" className={styles.img} />
+        <img src="/logoWhite.png" alt="Telenor" className={styles.img} />
     </div>
 );
 
