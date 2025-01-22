9de0b916b322b03ab6d8b5ee9508c4d7d6d92a8c
Tiril Hvidsand <tiril.hvidsand@telenor.no>
2025-01-22 10:24:50 +0100

    logo and positioning

--- src/components/LogoDisplay/index.js
+++ src/components/LogoDisplay/index.js
@@ -1,9 +1,10 @@
 import React from "react";
+import * as styles from "./LogoDisplay.module.css"
 
 const LogoDisplay = () => (
     <div>
         <div>
-            <img src="/logo.png" alt="Telenor " />
+            <img src="/logo.png" alt="Telenor" className={styles.img} />
         </div>
     </div>
 );
