f9e6069469b113875fd67a7fb650ea80ac050f98
Eline Henriksen <eline.henriksen@telenor.no>
2025-01-23 13:46:21 +0100

    Add title to tech blog pagw

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -27,7 +27,7 @@ const BlogIndex = ({ data, location }) => {
   return (
     <Layout location={location} title={siteTitle}>
       <Container className={styles.blogList}>
-        <Bio />
+        <h1>The Nova Tech Blog</h1>
         <ol style={{ listStyle: `none` }}>
           {posts.map(post => {
             const title = post.frontmatter.title || post.fields.slug
