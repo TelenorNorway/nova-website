23f6944b2ebd96d8506dcc6ebcc10f802b3098af
Eline Henriksen <eline.henriksen@telenor.no>
2025-01-23 09:55:52 +0100

    Update prefix

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,7 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-  pathPrefix: "/s07373-nova-website",
+  pathPrefix: "/nova-website",
   siteMetadata: {
     title: 'Nova',
     author: {
