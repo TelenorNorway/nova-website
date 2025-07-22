17c848e6b47d3162dcc77b323dbb36654fc0834f
Eline Henriksen <eline.henriksen@telenor.no>
2025-07-22 15:04:27 +0200

    Add sharing metadata on blog for social media

--- src/main.jsx
+++ src/main.jsx
@@ -2,22 +2,25 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { HelmetProvider } from 'react-helmet-async'
 import { PostHogProvider } from 'posthog-js/react'
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <PostHogProvider
-      apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY}
-      options={{
-        api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
-        defaults: '2025-05-24',
-        capture_exceptions: true,
-        debug: import.meta.env.MODE === 'development',
-        opt_out_capturing_by_default: true,
-        disable_surveys: true,
-      }}
-    >
-      <App />
-    </PostHogProvider>
-  </StrictMode>,
-)
+    <HelmetProvider>
+      <PostHogProvider
+        apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY}
+        options={{
+          api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+          defaults: "2025-05-24",
+          capture_exceptions: true,
+          debug: import.meta.env.MODE === "development",
+          opt_out_capturing_by_default: true,
+          disable_surveys: true,
+        }}
+      >
+        <App />
+      </PostHogProvider>
+    </HelmetProvider>
+  </StrictMode>
+);
--- src/pages/Blog/index.jsx
+++ src/pages/Blog/index.jsx
@@ -1,18 +1,46 @@
-import { useParams } from 'react-router';
-import * as blogs from '../../../blog';
+import { useParams } from "react-router";
+import * as blogs from "../../../blog";
 import BlogComponent from "../../components/Blog";
 import Layout from "../../components/Layout/index.jsx";
+import { Helmet } from "react-helmet-async";
 
 const Blog = () => {
-    const params = useParams();
+  const params = useParams();
+  const slug = params.slug;
+  const blog = Object.values(blogs).find((blog) => blog.metadata.slug === slug);
 
-    const slug = params.slug;
-    const blog = Object.values(blogs).find(blog => blog.metadata.slug === slug);
-    return (
-        <Layout>
-            <BlogComponent blog={blog} />
-        </Layout>
-    );
-}
+  console.log(process.env);
+
+  return (
+    <Layout>
+      <Helmet>
+        <meta property="og:title" content={blog.title} />
+        <meta property="og:url" content={`http://telenornorway.github.io/nova-website/blog/${slug}`} />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:description"
+          content={blog.metadata.description}
+        />
+        <meta
+          property="og:image"
+          content={`http://telenornorway.github.io/nova-website${blog.metadata.image}`}
+        />
+
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:title" content={blog.title} />
+        <meta
+          name="twitter:description"
+          content={blog.metadata.description}
+        />
+        <meta name="twitter:url" content={`http://telenornorway.github.io/nova-website/blog/${slug}`} />
+        <meta
+          name="twitter:image"
+          content={`http://telenornorway.github.io/nova-website${blog.metadata.image}`}
+        />
+      </Helmet>
+      <BlogComponent blog={blog} />
+    </Layout>
+  );
+};
 
 export default Blog;
