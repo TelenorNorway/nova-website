ee072c50230d39228ea153caf1b4d6d470cadc16
Eline Henriksen <eline.henriksen@telenor.no>
2025-09-18 15:13:20 +0200

    Add links from front page to sections on features

--- src/pages/Landing/Features/index.jsx
+++ src/pages/Landing/Features/index.jsx
@@ -1,31 +1,51 @@
-import GlassmorphicBox from '../../../components/GlassmorphicBox';
+import GlassmorphicBox from "../../../components/GlassmorphicBox";
 import { Rocket, Lock, SatelliteDish } from "lucide-react";
-import { container, features, feature, featureIcon, featureHeading, featureParagraph } from './styles.css';
+import {
+  container,
+  features,
+  feature,
+  featureIcon,
+  featureHeading,
+  featureParagraph,
+} from "./styles.css";
 
 const Feature = ({ icon, heading, to = "features", children }) => (
-    <GlassmorphicBox className={feature} to={to}>
-        <div className={featureIcon}>{icon}</div>
-        <h3 className={featureHeading}>{heading}</h3>
-        <p className={featureParagraph}>{children}</p>
-    </GlassmorphicBox>
+  <GlassmorphicBox className={feature} to={to}>
+    <div className={featureIcon}>{icon}</div>
+    <h3 className={featureHeading}>{heading}</h3>
+    <p className={featureParagraph}>{children}</p>
+  </GlassmorphicBox>
 );
 
 const Features = () => {
-    return (
-        <section className={container} id="features" style={{ paddingTop: 0 }}>
-            <div className={features}>
-                <Feature icon={<Rocket width={48} height={48} />} heading="Zero‑to‑prod in minutes">
-                    Get your applications to customers quickly and reliably
-                </Feature>
-                <Feature icon={<Lock width={48} height={48} />} heading="Move fast and feel safe">
-                    As a company managing critical infrastructure, Telenor has a proven security posture
-                </Feature>
-                <Feature icon={<SatelliteDish width={48} height={48} />} heading="Catalyst for organizational transformation">
-                    Transform how your organization approaches software development
-                </Feature>
-            </div>
-        </section>
-    );
+  return (
+    <section className={container} id="features" style={{ paddingTop: 0 }}>
+      <div className={features}>
+        <Feature
+          to="features#self-service-infrastructure"
+          icon={<Rocket width={48} height={48} />}
+          heading="Zero‑to‑prod in minutes"
+        >
+          Get your applications to customers quickly and reliably
+        </Feature>
+        <Feature
+          to="features#devsecops"
+          icon={<Lock width={48} height={48} />}
+          heading="Move fast and feel safe"
+        >
+          As a company managing critical infrastructure, Telenor has a proven
+          security posture
+        </Feature>
+        <Feature
+          to="features#governance"
+          icon={<SatelliteDish width={48} height={48} />}
+          heading="Catalyst for organizational transformation"
+        >
+          Transform how your organization approaches software development
+        </Feature>
+      </div>
+    </section>
+  );
 };
 
 export default Features;
