17435135b3a94a733c47ac6cec4dfb38c1ff22f3
Eline Henriksen <eline.henriksen@telenor.no>
2025-09-19 12:19:42 +0200

    Accidentally forgot to enable tracking again

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,11 +1,30 @@
-import { Head } from 'vite-react-ssg'
+import { Head } from "vite-react-ssg";
 import cx from "classnames";
+import { PostHogProvider } from "posthog-js/react";
 import Header from "../Header";
 import Footer from "../Footer";
 import Banner from "../ConsentBanner";
 import { container, innerContainer } from "./styles.css.ts";
 
-const Layout = ({ className, children, ...props }) => {
+export default function Layout(props) {
+  return (
+    <PostHogProvider
+      apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY}
+      options={{
+        api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+        defaults: "2025-05-24",
+        capture_exceptions: true,
+        debug: import.meta.env.MODE === "development",
+        opt_out_capturing_by_default: true,
+        disable_surveys: true,
+      }}
+    >
+      <LayoutContent {...props} />
+    </PostHogProvider>
+  );
+}
+
+const LayoutContent = ({ className, children, ...props }) => {
   return (
     <>
       <Head>
@@ -23,5 +42,3 @@ const Layout = ({ className, children, ...props }) => {
     </>
   );
 };
-
-export default Layout;
