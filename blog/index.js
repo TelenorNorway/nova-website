35efde8559f55e50d8bcc482bd2785f491674bc7
Eline Henriksen <eline.henriksen@telenor.no>
2025-11-20 10:23:13 +0100

    Prepend 0 to blog directories to ensure stable ordering

--- blog/index.js
+++ blog/index.js
@@ -1,5 +1,5 @@
-export * as FaceForward from './5-face-forward/index.mdx';
-export * as CloudNativeBergen from './4-bergen/index.mdx';
-export * as OffentligPaas from './3-offentlig-paas/index.mdx';
-export * as InnovationDays from './2-innovation-days/index.mdx';
-export * as Launch from './1-launch/index.mdx';
+export * as FaceForward from './05-face-forward/index.mdx';
+export * as CloudNativeBergen from './04-bergen/index.mdx';
+export * as OffentligPaas from './03-offentlig-paas/index.mdx';
+export * as InnovationDays from './02-innovation-days/index.mdx';
+export * as Launch from './01-launch/index.mdx';
