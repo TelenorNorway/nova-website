2b9427748726e7d08482eed980d2fe33e6b8065e
Eline Henriksen <eline.henriksen@telenor.no>
2025-12-05 14:51:08 +0100

    Add the shai hulud blog post to the list

--- blog/index.js
+++ blog/index.js
@@ -1,3 +1,4 @@
+export * as CloudSecurityShaiHulud from './07-cloud-security-shai-hulud/index.mdx';
 export * as SecurityResearcherSpotlight from './06-security-researcher-spotlight/index.mdx';
 export * as FaceForward from './05-face-forward/index.mdx';
 export * as CloudNativeBergen from './04-bergen/index.mdx';
