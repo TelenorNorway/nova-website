bcf733ad4f78f676d317a8696e3475417604c464
Karim El-Melhaoui <26272119+karimelmel@users.noreply.github.com>
2025-12-05 14:13:17 +0100

    chore: new blog post cloud security shai-hulud (#6)

--- blog/authors.js
+++ blog/authors.js
@@ -1,5 +1,6 @@
 import eline from './authors/eline.png';
 import mats from './authors/mats.png';
+import karim from './authors/karim.png';
 
 const authors = {
   "eline": {
@@ -13,6 +14,12 @@ const authors = {
     role: "Value Stream Lead - Cloud Platforms",
     image: mats,
     link: "https://www.linkedin.com/in/mats-andr%C3%A9-b%C3%A6kkelund-2b16a63", 
+  },
+  "karim": {
+    name: "Karim El-Melhaoui",
+    role: "Cloud Security Tech Lead - Cloud Platforms",
+    image: karim,
+    link: "https://www.linkedin.com/in/karim-el-melhaoui/", 
   },
 };
 
